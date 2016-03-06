45805e7b395842206f899730d5fb2d4c52b4c83b
svcperf <andytest.sec@googlemail.com>
2016-03-07 00:11:34 +0900

    Update sw.js
    debugging..

--- sw.js
+++ sw.js
@@ -5,7 +5,8 @@ this.addEventListener('install', function(event) {
         '/svcworker/',
         '/svcworker/index.html',
         '/svcworker/style.css',
-        '/svcworker/app.js'
+        '/svcworker/app.js',
+        '/svcworker/Service.jpg'
       ]);
     })
   );
@@ -15,7 +16,8 @@ this.addEventListener('activate', function(event) {
   event.waitUntil(this.clients.claim().then(function() {
     // See https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
     console.log('matchAll executing..');
-    return this.clients.matchAll({type: 'wearable'});
+    // return this.clients.matchAll({type: 'wearable'});
+    return this.clients.matchAll({type: 'window'});
   }).then(function(clients) {
     return clients.map(function(client) {
       if ('postmessage' in client) {
