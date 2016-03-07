14f93b5e80ba2bebc3eb273d9e374946409e0c82
svcperf <andytest.sec@googlemail.com>
2016-03-07 23:24:58 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ this.addEventListener('activate', function(event) {
   .then(function(clients) {
     clients.forEach(function(client) {
       console.log('postMessage executing..');
-      client.postMessage(msg);
+      client.postMessage('open'+'m.naver.com');
     })
   }));
 });
