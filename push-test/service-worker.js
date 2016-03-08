4fe370a2d22f6c10cb37873bf67ce4498d7ee77f
svcperf <andytest.sec@googlemail.com>
2016-03-08 17:36:32 +0900

    Update service-worker.js

--- push-test/service-worker.js
+++ push-test/service-worker.js
@@ -38,7 +38,7 @@ self.addEventListener('notificationclick', function(event) {
         return client.focus();
     }
     if (clients.openWindow)      
-      var url = '/svcworker/push-test/index.html';    
+      var url = '/chrome-push/index.html';    
       return clients.openWindow(url);
   }));
 });
