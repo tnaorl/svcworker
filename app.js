5ed1ae0f6457772c59f0b0b9ae5da4f116c1b28b
svcperf <andytest.sec@googlemail.com>
2016-03-09 20:15:03 +0900

    Update app.js
    .

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 // register service worker
 var registration;
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/svcworker/sw.js', { scope: '/svcworker/' })
-    .then(function(reg) {
-      registration = reg;
-    
-      if(reg.installing) {
-        console.log('Service worker installing');
-      } else if(reg.waiting) {
-        console.log('Service worker installed');
-      } else if(reg.active) {
-        console.log('Service worker active');
-      }
-    })
-    .catch(function(error) {
-      // registration failed
-      console.log('Registration failed with ' + error);
-    });
-};
+function register() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/svcworker/sw.js', { scope: '/svcworker/' })
+      .then(function(reg) {
+        registration = reg;
+
+        if(reg.installing) {
+          console.log('Service worker installing');
+        } else if(reg.waiting) {
+          console.log('Service worker installed');
+        } else if(reg.active) {
+          console.log('Service worker active');
+        }
+      })
+      .catch(function(error) {
+        // registration failed
+        console.log('Registration failed with ' + error);
+      });
+  };
+}
 
 function unregister() {
   registration.unregister()
